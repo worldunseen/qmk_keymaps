ffcbdaa7c79d3cc4bf74a76cf013337c790be8e9
worldunseen <world@unseen.dev>
2022-06-04 23:03:08 -0400

    init planck config

---
+++ planck/config.h
@@ -0,0 +1,29 @@
+/* Copyright 2015-2021 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifdef AUDIO_ENABLE
+#    define STARTUP_SONG SONG(PLANCK_SOUND)
+// #define STARTUP_SONG SONG(NO_SOUND)
+#endif
+
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TERM 135
+
+// High polling + chording
+#define QMK_KEYS_PER_SCAN 12
+#define USB_POLLING_INTERVAL_MS 1
