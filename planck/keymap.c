84c452924d7bc04b6a2dafe32b9d9f8819b090ff
worldunseen <world@unseen.dev>
2022-06-05 15:44:40 -0400

    more around modifiers and add more navigation keys

--- planck/keymap.c
+++ planck/keymap.c
@@ -44,13 +44,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,         KC_E,         KC_R,         KC_T,    KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,    KC_BSPC,
-    KC_ESC,  KC_A,    LSFT_T(KC_S), LALT_T(KC_D), LCTL_T(KC_F), KC_G,    KC_H,   RCTL_T(KC_J), RALT_T(KC_K), RSFT_T(KC_L), KC_SCLN, KC_QUOT,
-    _______, KC_Z,    KC_X,         KC_C,         LGUI_T(KC_V), KC_B,    KC_N,   RGUI_T(KC_M), KC_COMM,      KC_DOT,       KC_SLSH, KC_ENT,
+    KC_ESC,  KC_A,    LSFT_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,    KC_H,   RGUI_T(KC_J), RALT_T(KC_K), RSFT_T(KC_L), KC_SCLN, KC_QUOT,
+    _______, KC_Z,    KC_X,         KC_C,         LCTL_T(KC_V), KC_B,    KC_N,   RCTL_T(KC_M), KC_COMM,      KC_DOT,       KC_SLSH, KC_ENT,
     _______, _______, _______,      NAV,          LOWER,        _______, KC_SPC, RAISE,        _______,      _______,      _______, _______
 ),
 
 [_NAVIGATION] = LAYOUT_planck_grid(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_PGUP, KC_HOME, KC_END,  KC_PGDN, _______, _______,
     _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
