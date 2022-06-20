7ddd454467059453390303166235c7c4a83bc47c
worldunseen <world@unseen.dev>
2022-06-20 08:18:34 -0400

    various updates

--- planck/keymap.c
+++ planck/keymap.c
@@ -19,6 +19,7 @@
 
 enum planck_layers {
   _QWERTY,
+  _GAMING,
   _NAVIGATION,
   _LOWER,
   _RAISE,
@@ -46,11 +47,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,         KC_E,         KC_R,         KC_T,    KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    LSFT_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,    KC_H,   RGUI_T(KC_J), RALT_T(KC_K), RSFT_T(KC_L), KC_SCLN, KC_QUOT,
     _______, KC_Z,    KC_X,         KC_C,         LCTL_T(KC_V), KC_B,    KC_N,   RCTL_T(KC_M), KC_COMM,      KC_DOT,       KC_SLSH, KC_ENT,
-    _______, _______, _______,      NAV,          LOWER,        _______, KC_SPC, RAISE,        _______,      _______,      _______, _______
+    _______, _______, _______,      NAV,          LOWER,        _______, KC_SPC, RAISE,        _______,      _______,      _______, TG(_GAMING)
+),
+
+/* Qwerty
+* ,-----------------------------------------------------------------------------------.
+* | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+* |------+------+------+------+------+------+------+------+------+------+------+------|
+* | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
+* |------+------+------+------+------+------+------+------+------+------+------+------|
+* | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+* |------+------+------+------+------+------+------+------+------+------+------+------|
+* | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+* `-----------------------------------------------------------------------------------'
+ */
+[_GAMING] = LAYOUT_planck_grid(
+    KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,  KC_T,    KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_ESC,  KC_A,    KC_S,    KC_D,   KC_F,  KC_G,    KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,  KC_B,    KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+    _______, KC_LCTL, KC_LALT, KC_SPC, LOWER, KC_LGUI, KC_SPC, RAISE, _______, _______, _______, TG(_GAMING)
 ),
 
 [_NAVIGATION] = LAYOUT_planck_grid(
-    _______, _______, _______, _______, _______, _______, KC_PGUP, KC_HOME, KC_END,  KC_PGDN, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
@@ -110,7 +129,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 )
-
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
